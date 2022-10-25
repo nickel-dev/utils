ebac79c25ff950c10f0885c5e9f9245503e8adf5
Nickel-Dev <92646300+nickel-dev@users.noreply.github.com>
2022-10-25 18:11:57 +0200

    Add files via upload

---
+++ base_types.h
@@ -0,0 +1,22 @@
+#ifndef BASE_TYPES_H_
+#define BASE_TYPES_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef int8_t    i8    ;
+typedef int16_t   i16   ;
+typedef int32_t   i32   ;
+typedef int64_t   i64   ;
+typedef uint8_t   ui8   ;
+typedef uint16_t  ui16  ;
+typedef uint32_t  ui32  ;
+typedef uint64_t  ui64  ;
+typedef i8        b8    ;
+typedef i16       b16   ;
+typedef i32       b32   ;
+typedef i64       b64   ;
+typedef float     f32   ;
+typedef double    f64   ;
+
+#endif // BASE_TYPES_H_
